70fd747bd18529ff2c66cc093e5be452e7e1a19b
Jay Pearson <jay.pearson@gmail.com>
2019-10-18 13:35:45 -0400

    Fixes

--- csharpExamples/Program.cs
+++ csharpExamples/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
+using MongoDB.Bson;
 using MongoDB.Driver;
-using Utf8Json;
 
 namespace csharpExamples
 {
@@ -9,9 +9,46 @@ namespace csharpExamples
     {
         static void Main(string[] args)
         {
-            var filename = "twitter.json";
-            var jsonString = JsonSerializer.ToJsonString(File.ReadAllBytes(filename));
             var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("test");
+            var collection = db.GetCollection<BsonDocument>("test");
+            var models = new List<WriteModel<BsonDocument>>();
+            for (int i = 0; i < 10; i++)
+            {
+                var doc = new BsonDocument("_id", i);
+                var model = new UpdateOneModel<BsonDocument>(doc, new BsonDocument("$set", new BsonDocument("a", i * 2)));
+                var insertModel = new InsertOneModel<BsonDocument>(doc);
+                model.IsUpsert = true;
+                models.Add(model);
+                if (i % 5 == 0)
+                {
+                    models.Add(insertModel);
+                }
+            }
+            try
+            {
+                var result = collection.BulkWrite(models, new BulkWriteOptions { IsOrdered = false });
+            }
+            catch (MongoWriteConcernException mwce)
+            {
+                var result = mwce.WriteConcernResult;
+
+                Console.Out.WriteLine(mwce.Message);
+            }
+            catch (MongoBulkWriteException mbwe)
+            {
+                foreach (var error in mbwe.WriteErrors)
+                {
+                    int index = error.Index;
+                    string message = error.Message;
+                    int code = error.Code;
+                    var category = error.Category;
+                    category.Equals(ServerErrorCategory.DuplicateKey);
+                    category.Equals(ServerErrorCategory.ExecutionTimeout);
+                    Console.Out.WriteLine($"Index #{index} - {message} - code {code} - category {category}");
+                }
+            }
+
         }
     }
 }
